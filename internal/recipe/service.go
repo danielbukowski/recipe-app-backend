099b06756ee7443e7be326e520332be7e234f5e7
Daniel Bukowski <daniel.bukowski01@proton.me>
2024-12-09 16:21:24 +0100

    Add a method to get Recipe by id in the service

--- internal/recipe/service.go
+++ internal/recipe/service.go
@@ -1,8 +1,12 @@
 package recipe
 
 import (
+	"context"
+	"errors"
 	"time"
 
+	"github.com/danielbukowski/recipe-app-backend/gen/sqlc"
+	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"go.uber.org/zap"
 )
@@ -20,3 +24,22 @@ func NewService(logger *zap.Logger, dbpool *pgxpool.Pool) *service {
 		dbpool: dbpool,
 	}
 }
+
+func (s *service) GetRecipeById(ctx context.Context, recipeId uuid.UUID) (sqlc.Recipe, error) {
+	poolCtx, cancel := context.WithTimeout(ctx, databaseConnectionTimeout)
+	defer cancel()
+
+	conn, err := s.dbpool.Acquire(poolCtx)
+	if err != nil {
+		return sqlc.Recipe{}, errors.New("failed to connect to database")
+	}
+	defer conn.Release()
+
+	q := sqlc.New(conn)
+
+	dbCtx, cancel := context.WithTimeout(ctx, databaseConnectionTimeout)
+	defer cancel()
+
+	return q.GetRecipeById(dbCtx, recipeId)
+}
+
