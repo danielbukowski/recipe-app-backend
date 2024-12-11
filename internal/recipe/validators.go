58db82cb2fd0208cf55add257ac5cfe14cf14869
Daniel Bukowski <daniel.bukowski01@proton.me>
2024-12-11 23:48:57 +0100

    Fix the validator

--- internal/recipe/validators.go
+++ internal/recipe/validators.go
@@ -1,13 +1,15 @@
 package recipe
 
-import "github.com/danielbukowski/recipe-app-backend/internal/validator"
+import (
+	"github.com/danielbukowski/recipe-app-backend/internal/validator"
+)
 
 type newRecipeRequest struct {
-	title   string
-	content string
+	Title   string `json:"title"`
+	Content string `json:"content"`
 }
 
 func validateRecipe(v *validator.Validator, requestBody newRecipeRequest) {
-	v.Check(len(requestBody.content) <= 10, "content", "should be at least 10 characters length long")
-	v.Check(len(requestBody.title) <= 10, "title", "should be at least 10 characters length long")
+	v.Check(len(requestBody.Content) >= 10, "content", "should be at least 10 characters length long")
+	v.Check(len(requestBody.Title) >= 10, "title", "should be at least 10 characters length long")
 }
