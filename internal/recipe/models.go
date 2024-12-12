940e544e6a8faa8e3cbed4f15006a0a80b2ba0d5
Daniel Bukowski <daniel.bukowski01@proton.me>
2024-12-12 23:24:32 +0100

    Add recipeResponse struct to model.go file

--- internal/recipe/models.go
+++ internal/recipe/models.go
@@ -2,6 +2,13 @@ package recipe
 
 import "time"
 
+type recipeResponse struct {
+	Title     string    `json:"title"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
 type newRecipeRequest struct {
 	Title   string `json:"title"`
 	Content string `json:"content"`
