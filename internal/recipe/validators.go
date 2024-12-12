f7f572bdbbf656542eeaee4dd70233073af42515
Daniel Bukowski <daniel.bukowski01@proton.me>
2024-12-12 23:16:34 +0100

    Move newRecipeRequest to model.go file

--- internal/recipe/validators.go
+++ internal/recipe/validators.go
@@ -4,11 +4,6 @@ import (
 	"github.com/danielbukowski/recipe-app-backend/internal/validator"
 )
 
-type newRecipeRequest struct {
-	Title   string `json:"title"`
-	Content string `json:"content"`
-}
-
 func validateRecipe(v *validator.Validator, requestBody newRecipeRequest) {
 	v.Check(len(requestBody.Content) >= 10, "content", "should be at least 10 characters length long")
 	v.Check(len(requestBody.Title) >= 10, "title", "should be at least 10 characters length long")
