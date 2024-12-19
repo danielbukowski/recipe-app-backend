3e23054878591ff583903a0574bda0dc7407209c
Daniel Bukowski <daniel.bukowski01@proton.me>
2024-12-19 19:25:48 +0100

    Comment RegisterRoutes method

--- internal/recipe/routes.go
+++ internal/recipe/routes.go
@@ -4,8 +4,8 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// RegisterRoutes sets endpoints for Recipe resource.
 func (h *handler) RegisterRoutes(gin *gin.Engine) {
-
 	gin.POST("api/v1/recipes", h.createRecipe)
 	gin.GET("api/v1/recipes/:id", h.getRecipeById)
 	gin.PUT("api/v1/recipes/:id", h.updateRecipeById)
