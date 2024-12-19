c2fa6fa87e88e352f32460c755760371dd77e365
Daniel Bukowski <daniel.bukowski01@proton.me>
2024-12-19 23:36:29 +0100

    Refactor validate function

--- internal/recipe/validators.go
+++ internal/recipe/validators.go
@@ -1,10 +1,17 @@
 package recipe
 
 import (
+	"fmt"
+
 	"github.com/danielbukowski/recipe-app-backend/internal/validator"
 )
 
+const (
+	minTitleLength   = 4
+	minContentLength = 10
+)
+
 func validateNewRecipeRequestBody(v *validator.Validator, requestBody newRecipeRequest) {
-	v.Check(len(requestBody.Content) >= 10, "content", "should be at least 10 characters length long")
-	v.Check(len(requestBody.Title) >= 10, "title", "should be at least 10 characters length long")
+	v.Check(len(requestBody.Content) >= minContentLength, "content", fmt.Sprintf("should be at least %d characters length long", minContentLength))
+	v.Check(len(requestBody.Title) >= minTitleLength, "title", fmt.Sprintf("should be at least %d characters length long", minTitleLength))
 }
