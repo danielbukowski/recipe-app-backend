63858818daeca486460ef16599ecdaac03395622
Daniel Bukowski <daniel.bukowski01@proton.me>
2025-01-15 18:49:05 +0100

    Change the params field of the argonPasswordHasher to a pointer

--- internal/argon/password_hasher.go
+++ internal/argon/password_hasher.go
@@ -5,15 +5,15 @@ import (
 )
 
 type argonPasswordHasher struct {
-	params argon2id.Params
+	params *argon2id.Params
 }
 
-func New(params argon2id.Params) *argonPasswordHasher {
+func New(params *argon2id.Params) *argonPasswordHasher {
 	return &argonPasswordHasher{
 		params: params,
 	}
 }
 
 func (h *argonPasswordHasher) CreateHashFromPassword(password string) (string, error) {
-	return argon2id.CreateHash(password, &h.params)
+	return argon2id.CreateHash(password, h.params)
 }
