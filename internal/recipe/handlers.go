774dd100762e1ddcc184b118c33d4d29f3378908
Daniel Bukowski <daniel.bukowski01@proton.me>
2025-01-19 19:12:23 +0100

    Rewrite deleteRecipeById handler

--- internal/recipe/handlers.go
+++ internal/recipe/handlers.go
@@ -159,53 +159,33 @@ func (h *handler) updateRecipeById(c echo.Context) error {
 //	@Failure		500	{object}	shared.CommonResponse
 //
 //	@Router			/api/v1/recipes/{id} [DELETE]
-func (h *handler) deleteRecipeById(ctx *gin.Context) {
-	recipeIdParam, ok := ctx.Params.Get("id")
+func (h *handler) deleteRecipeById(c echo.Context) error {
+	recipeIdParam := c.Param("id")
 
-	if !ok {
-		ctx.JSON(http.StatusBadRequest, gin.H{
-			"message": "missing ID param for recipe",
-		})
-		return
+	if recipeIdParam == "" {
+		return c.JSON(http.StatusBadRequest, shared.CommonResponse{Message: "missing ID param for recipe"})
 	}
 
 	recipeId, err := uuid.Parse(recipeIdParam)
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{
-			"message": "the received ID is not a valid UUID",
-		})
-		return
+		return c.JSON(http.StatusBadRequest, shared.CommonResponse{Message: "the received ID is not a valid UUID"})
 	}
 
-	err = h.recipeService.DeleteRecipeById(ctx.Copy(), recipeId)
+	err = h.recipeService.DeleteRecipeById(c.Request().Context(), recipeId)
 	if err != nil {
 		switch {
 		case errors.Is(err, pgx.ErrNoRows):
-			ctx.Status(http.StatusNoContent)
-		case errors.Is(err, context.DeadlineExceeded):
-			ctx.JSON(http.StatusRequestTimeout, gin.H{
-				"message": "failed to delete a recipe in time",
-			})
+			return echo.NewHTTPError(http.StatusNoContent)
 		default:
-			ctx.JSON(http.StatusInternalServerError, gin.H{
-				"message": http.StatusText(http.StatusInternalServerError),
-			})
-
-			h.logger.Error(err.Error(),
-				zap.Stack("stackError"),
-				zap.String("recipeId", recipeId.String()),
-				zap.String("method", ctx.Request.Method),
-				zap.String("path", ctx.FullPath()),
-			)
+			// TODO: think about returning here a http internal error or think how to handle this in the global exception handler
+			return err
 		}
-		return
+
 	}
 
-	h.logger.Info("deleted a recipe from database",
-		zap.String("recipeId", recipeId.String()),
-	)
+	h.logger.Info("deleted a recipe from database", zap.String("recipeId", recipeId.String()))
 
-	ctx.Status(http.StatusNoContent)
+	return c.NoContent(http.StatusNoContent)
 }
 
 //	@Summary		Get a recipe
