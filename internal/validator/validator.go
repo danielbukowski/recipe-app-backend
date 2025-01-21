db6744b126d80033e4f11f47ab9829914a49cb2a
Daniel Bukowski <daniel.bukowski01@proton.me>
2025-01-21 23:13:03 +0100

    Rewrite the Validator

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -1,25 +1,56 @@
 package validator
 
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/labstack/echo/v4"
+)
+
+type validationError struct {
+	Message string            `json:"message"`
+	Fields  map[string]string `json:"fields"`
+}
+
 type Validator struct {
-	Errors map[string]string
+	validator *validator.Validate
 }
 
 func New() *Validator {
-	return &Validator{Errors: make(map[string]string)}
+	return &Validator{validator: validator.New()}
 }
 
-func (v *Validator) AddError(key, message string) {
-	if _, exists := v.Errors[key]; !exists {
-		v.Errors[key] = message
+func (cv *Validator) Validate(i interface{}) error {
+	if err := cv.validator.Struct(i); err != nil {
+
+		var vErr *validationError = &validationError{
+			Message: "Your request body did not pass the validation",
+			Fields:  make(map[string]string),
+		}
+
+		for _, err := range err.(validator.ValidationErrors) {
+			var message string
+
+			switch err.ActualTag() {
+			case "required":
+				message = "cannot be blank"
+			case "email":
+				message = "must be a valid email address"
+			case "eqfield":
+				message = fmt.Sprintf("must be the same as the %s field", err.Param())
+			case "min":
+				message = fmt.Sprintf("must be at least %v characters long", err.Param())
+			case "max":
+				message = fmt.Sprintf("cannot be more than %v characters long", err.Param())
+			default:
+				message = fmt.Sprintf("Field '%s': '%v' must satisfy '%s' '%v' criteria", err.Field(), err.Value(), err.Tag(), err.Param())
+			}
+
+			vErr.Fields[err.Field()] = message
+		}
+
+		return echo.NewHTTPError(http.StatusBadRequest, vErr)
 	}
-}
-
-func (v *Validator) Valid() bool {
-	return len(v.Errors) == 0
-}
-
-func (v *Validator) Check(ok bool, key, message string) {
-	if !ok {
-		v.AddError(key, message)
-	}
+	return nil
 }
