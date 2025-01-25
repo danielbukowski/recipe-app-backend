180dbe0c890827b05093710beb9060f4d0331278
Daniel Bukowski <daniel.bukowski01@proton.me>
2025-01-25 23:07:56 +0100

    Add SignInRequest struct

--- internal/auth/models.go
+++ internal/auth/models.go
@@ -5,3 +5,8 @@ type SignUpRequest struct {
 	Password      string `json:"password" validate:"required,min=5,max=50"`
 	PasswordAgain string `json:"password_again" validate:"required,eqfield=Password"`
 }
+
+type SignInRequest struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=5,max=50"`
+}
