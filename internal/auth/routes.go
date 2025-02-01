b734067e9c7484191ec1b7a81b186e2c47ee1287
Daniel Bukowski <daniel.bukowski01@proton.me>
2025-02-01 23:44:49 +0100

    Add an endpoint to sign out

--- internal/auth/routes.go
+++ internal/auth/routes.go
@@ -7,4 +7,5 @@ import (
 func (h *handler) RegisterRoutes(e *echo.Echo) {
 	e.POST("api/v1/auth/signup", h.SignUp)
 	e.POST("api/v1/auth/signin", h.signIn)
+	e.POST("api/v1/auth/signout", h.signOut)
 }
