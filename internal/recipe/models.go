6134f58fb23c0f63cf80ff2a10102cdc4f945644
Daniel Bukowski <daniel.bukowski01@proton.me>
2025-02-05 23:44:01 +0100

    Add example to the models of the recipe module

--- internal/recipe/models.go
+++ internal/recipe/models.go
@@ -3,18 +3,18 @@ package recipe
 import "time"
 
 type RecipeResponse struct {
-	Title     string    `json:"title"`
-	Content   string    `json:"content"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	Title     string    `json:"title" example:"Chocolate Cookies"`
+	Content   string    `json:"content" example:"Having all your ingredients the same temperature really helps here"`
+	CreatedAt time.Time `json:"created_at" example:"2025-02-05T21:35:31.00635Z"`
+	UpdatedAt time.Time `json:"updated_at"  example:"2025-02-07T21:35:31.00635Z"`
 }
 
 type NewRecipeRequest struct {
-	Title   string `json:"title" validate:"required,min=5"`
-	Content string `json:"content" validate:"required,min=5"`
+	Title   string `json:"title" validate:"required,min=5" example:"Chocolate Cookies"`
+	Content string `json:"content" validate:"required,min=5" example:"Having all your ingredients the same temperature really helps here"`
 }
 
 type UpdateRecipeRequest struct {
-	Title   string `json:"title" validate:"required,min=5"`
-	Content string `json:"content" validate:"required,min=5"`
+	Title   string `json:"title" validate:"required,min=5" example:"Chocolate Cookies"`
+	Content string `json:"content" validate:"required,min=5" example:"Having all your ingredients the same temperature really helps here"`
 }
