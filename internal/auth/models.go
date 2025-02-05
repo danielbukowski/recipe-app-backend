0aec7690ddd80ee692c1823ab5dc0a6a2cadd5db
Daniel Bukowski <daniel.bukowski01@proton.me>
2025-02-06 00:02:58 +0100

    Add swag docs to the auth module

--- internal/auth/models.go
+++ internal/auth/models.go
@@ -1,16 +1,16 @@
 package auth
 
 type SignUpRequest struct {
-	Email         string `json:"email" validate:"required,email"`
-	Password      string `json:"password" validate:"required,min=5,max=50"`
-	PasswordAgain string `json:"password_again" validate:"required,eqfield=Password"`
+	Email         string `json:"email" validate:"required,email" example:"user@mail.com"`
+	Password      string `json:"password" validate:"required,min=5,max=50" example:"supersecretpassword"`
+	PasswordAgain string `json:"password_again" validate:"required,eqfield=Password"  example:"supersecretpassword"`
 }
 
 type SignInRequest struct {
-	Email    string `json:"email" validate:"required,email"`
-	Password string `json:"password" validate:"required,min=5,max=50"`
+	Email    string `json:"email" validate:"required,email"  example:"user@mail.com"`
+	Password string `json:"password" validate:"required,min=5,max=50"  example:"supersecretpassword"`
 }
 
 type SignInResponse struct {
-	Email string `json:"email"`
+	Email string `json:"email"  example:"user@mail.com"`
 }
