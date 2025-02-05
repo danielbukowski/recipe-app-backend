2d8fe1819fdd4ff8c52bd486cf0883f009667eed
Daniel Bukowski <daniel.bukowski01@proton.me>
2025-02-05 23:42:38 +0100

    Add swag docs to the checkHealth handler

--- internal/healthcheck/handlers.go
+++ internal/healthcheck/handlers.go
@@ -13,6 +13,13 @@ func NewHandler() *handler {
 	return &handler{}
 }
 
+//	@Summary		Check health
+//	@Description	Check the status of the recipe API.
+//	@Tags			health
+//
+//	@Success		200	"The API is healthy."
+//
+//	@Router			/api/v1/healthcheck [GET]
 func (h *handler) checkHealth(c echo.Context) error {
 	return c.NoContent(http.StatusOK)
 }
