29b635e6badb6ee81d0e1c4e55fde00a8504c610
Daniel Bukowski <daniel.bukowski01@proton.me>
2025-02-06 15:26:38 +0100

    Format swag docs

--- internal/auth/models.go
+++ internal/auth/models.go
@@ -3,14 +3,14 @@ package auth
 type SignUpRequest struct {
 	Email         string `json:"email" validate:"required,email" example:"user@mail.com"`
 	Password      string `json:"password" validate:"required,min=5,max=50" example:"supersecretpassword"`
-	PasswordAgain string `json:"password_again" validate:"required,eqfield=Password"  example:"supersecretpassword"`
+	PasswordAgain string `json:"password_again" validate:"required,eqfield=Password" example:"supersecretpassword"`
 }
 
 type SignInRequest struct {
-	Email    string `json:"email" validate:"required,email"  example:"user@mail.com"`
-	Password string `json:"password" validate:"required,min=5,max=50"  example:"supersecretpassword"`
+	Email    string `json:"email" validate:"required,email" example:"user@mail.com"`
+	Password string `json:"password" validate:"required,min=5,max=50" example:"supersecretpassword"`
 }
 
 type SignInResponse struct {
-	Email string `json:"email"  example:"user@mail.com"`
+	Email string `json:"email" example:"user@mail.com"`
 }
